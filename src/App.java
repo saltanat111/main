97922a94102c625f945b81aa9ef594017e52b2dc
Saltanat Kydykbekova <saltanat.kydykbekova1@gmail.com>
2023-12-01 15:26:34 +0600

    hi

--- src/App.java
+++ src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int o =0;
+    System.out.println("Hello, World!");
+        int o =0;
     }
-}.
+}
+ 
